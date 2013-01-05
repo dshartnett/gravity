9fe49b31776121cf30c6b3eac96624c50db7b88e
dshartnett <dshartnett@gmail.com>
2013-01-05 01:52:36 -0600

    derp

--- socket_worker.js
+++ socket_worker.js
@@ -2,7 +2,7 @@ importScripts("/socket.io/socket.io.js");
 
 var socket = io.connect("http://localhost:8080");
 
-//socket.on("pong", function (e) {postMessage(e);});
+socket.on("pong", function (e) {postMessage(e);});
 
 onmessage = function (e) {
 	if (e.data == "do something") postMessage(e.data);
